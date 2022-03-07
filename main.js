73da77babcef78cf65ee1c6891c1f6e843a4c400
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-03-07 14:57:54 +0100

    simple test API

--- main.js
+++ main.js
@@ -1,44 +1,47 @@
-// let data;
-// async function get(){
-//     const response  =await fetch("https://www.breakingbadapi.com/api/characters")
-//      data  = await response.json()
-//     console.log(data)
-//     document.querySelector('#content').innerHTML +=  data[1].name;
-//     document.querySelector('#content img').src =data[1].img
-//     document.querySelector('#select select').innerHTML= 
-//     `
-//     ${
-//         data.map(actor =>`  <option value="${actor.name}">${actor.name}</option>`)
-//     }
+let data 
+const API = "https://www.breakingbadapi.com/api/characters"
+async function get(){
+    const response  =await fetch(API)
+      data  = await response.json()
+     printData(data)
+    }
+
+
+
+
+    function printData(data){
+ 
+       
+        // document.querySelector('#content img').src =data[1].img
+        document.querySelector('#select select').innerHTML= 
+        `
+        ${
+            data.map(actor =>`  <option value="${actor.name}">${actor.name}</option>`)
+         }
+         
+         `
+     }
+get()
+function getch(value){
+    data.forEach(element => {
+        if(value == element.name){
+           document.querySelector('#content img').src =element.img 
+        }
+        
+    });
    
-//     `
-// }
-// const select  =document.querySelector('#select select')
-// function sel(){
-//     let val =select.value;
-//     console.log("is here");
-    
-//     data.filtter(actor =>{
+}
 
-//         if(val ==actor.name){
-
-//             console.log("is here2");
-//         }
-//     })
-    
-
-// }
-
-// get()
+ 
 
 
-const myPromise = new Promise((resolve, reject) => {
+// const myPromise = new Promise((resolve, reject) => {
 
-if (Math.random()* 100 < 50) resolve("Good");
-else reject("Bad");
-});
+// if (Math.random()* 100 < 50) resolve("Good");
+// else reject("Bad");
+// });
 
-myPromise.then(
-(resolved) => console.log(resolved),
-(rejected) => console.log(rejected)
-);
+// myPromise.then(
+// (resolved) => console.log(resolved),
+// (rejected) => console.log(rejected)
+// );
